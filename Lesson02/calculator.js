17db71e66c16364a8c4004c50bd4f55e6932f7e3
web media <59540946+jem1520@users.noreply.github.com>
2020-06-28 21:38:48 -0400

    added javascript for assignment 2 part 1

---
+++ Lesson02/calculator.js
@@ -0,0 +1,14 @@
+const d = 4;
+const h = 8;
+const a = 1;
+const b = 2;
+const e = 5;
+
+console.log(d + h);
+console.log(h * b);
+console.log(d - h * b);
+console.log(h * b - d);
+console.log(a + b + d - e);
+console.log(b / e);
+console.log((h / b) * h + a);
+console.log(b ** e - e);
