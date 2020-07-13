ed2128da33a703f66c873f194fd5c0552564b830
web media <59540946+jem1520@users.noreply.github.com>
2020-07-13 09:48:04 -0400

    adding javascript assignment 1

---
+++ Lesson03/exercise-one.js
@@ -0,0 +1,14 @@
+let fruit = ['strawberry', 'banana', 'kiwi', 'grape', 'peach', 'mango'];
+
+console.log(fruit);
+
+fruit.push('lemon');
+
+console.log(fruit);
+
+fruit.splice(2,1);
+
+console.log(fruit);
+
+let str = fruit.join(',');
+console.log(str);
