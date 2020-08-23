626846f2652635eee548d63591b657be0bae2582
web media <59540946+jem1520@users.noreply.github.com>
2020-08-23 19:23:13 -0400

    adding lesson 04 assignment 1

---
+++ Lesson04Assignment01/RegChallenge.js
@@ -0,0 +1,23 @@
+function squareNumber() {
+
+  let input = prompt('Enter a number');
+  let result = 0;
+
+  result = Math.pow(input, 2);
+
+  alert(result);
+
+}
+
+
+function multiplyNumber() {
+
+  let input1 = prompt('Enter first number');
+  let input2 = prompt('Enter second number');
+  let result = 0;
+
+  result = input1 * input2;
+
+  alert(result);
+
+}
